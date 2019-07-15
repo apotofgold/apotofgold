53610b868f125a8c6fad5b6f7ec308e78060219d
Danielle Gorman <gormandanielle13@gmail.com>
2019-07-15 16:26:02 +0100

    My first commit!

---
+++ users/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegistrationForm
+from django.contrib.auth.decorators import login_required
+def register(request):
+	#form = UserCreationForm(request.POST)
+	if request.method == 'POST':
+		form = UserRegistrationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			messages.success(request, f'Account created for {username}!')
+			return redirect('message-home')
+	else:
+		form = UserRegistrationForm()
+	return render(request, 'users/register.html',{'form':form})
+	
+@login_required
+def profile(request):
+	if request.method == 'POST':
+	    if request.user.userprofile is None:
+	        user_profile = UserProfile(user=request.user)
+	        user_profile.save
+	return render(request, 'users/profile.html')
