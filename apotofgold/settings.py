05f7807ed0c31722031b2c9cf6b33fddc844029c
Danielle Gorman <gormandanielle13@gmail.com>
2019-07-19 20:09:22 +0100

    add new3

--- apotofgold/settings.py
+++ apotofgold/settings.py
@@ -144,7 +144,7 @@ PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipmanifestSTATICFILESSTORAGE'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url
 prod_db = dj_database_url.config(conn_max_age=500)
