971d10766297f03d82ee7dee4d1d0e0425641b01
Danielle Gorman <gormandanielle13@gmail.com>
2019-07-19 19:34:16 +0100

    fuck another commit

--- apotofgold/settings.py
+++ apotofgold/settings.py
@@ -147,5 +147,5 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipmanifestSTATICFILESSTORAGE'
 
 import dj_database_url
-prod-db = dj_database_url.config(conn_max_age=500)
+prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
