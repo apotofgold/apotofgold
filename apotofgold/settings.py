6e5bbc6d20e052df7f133b99c6b64919f54de90d
Danielle Gorman <gormandanielle13@gmail.com>
2019-07-19 20:51:56 +0100

    add new8

--- apotofgold/settings.py
+++ apotofgold/settings.py
@@ -21,10 +21,10 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n(xni*3g-2+tzjr48176q^jmbx5$l#nb196z@hu-%j9x+h5gx_'
+SECRET_KEY = 'n(xni*3g-2+tzjr48176q^jmbx5$l#nb196z@hu-%j9x+h5gx_!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['apotofgoldeu.herokuapp.com']
 
