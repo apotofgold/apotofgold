2b7f04e39ec4b75ac3c802bc240de08d5dd8cf50
Danielle Gorman <gormandanielle13@gmail.com>
2019-07-19 20:01:05 +0100

    add new1

--- apotofgold/settings.py
+++ apotofgold/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['apotofgoldeu.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
-    
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
